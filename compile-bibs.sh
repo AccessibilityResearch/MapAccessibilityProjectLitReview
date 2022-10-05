2a45442a5028a3b4c8356ec615f20a6b2b1b8beb
Christian Stewart <heeler_cs@hotmail.com>
2022-10-05 10:25:17 -0500

    Added a bash script that compiles all our bibs into one file

---
+++ compile-bibs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+> main.bib
+cat ./Notes/christian/christian.bib >> main.bib
+echo "" >> main.bib
+cat ./Notes/Aaron/aaron.bib >> main.bib
+echo "" >> main.bib
+cat ./Notes/Aaron/aaron.bib >> main.bib
