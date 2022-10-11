879dd29d0151893527e7f42e1a41d54f7c5cdeb6
Aaron Cherney <aaron.cherney@go.minnstate.edu>
2022-10-11 15:28:44 -0500

    Fixed bash script bug, added Karter bibs->main.bib

--- compile-bibs.sh
+++ compile-bibs.sh
@@ -4,4 +4,4 @@ cat ./Notes/christian/christian.bib >> main.bib
 echo "" >> main.bib
 cat ./Notes/Aaron/aaron.bib >> main.bib
 echo "" >> main.bib
-cat ./Notes/Aaron/aaron.bib >> main.bib
+cat ./Notes/karter/karter.bib >> main.bib
